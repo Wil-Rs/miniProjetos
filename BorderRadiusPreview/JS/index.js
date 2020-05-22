00410bd4f0110bb59e6796368669a5dd5027c5fd
Wil-Rs <wilianrsantos.pro@gmail.com>
2020-05-21 23:48:50 -0500

    melhorado so um pouquinho

---
+++ BorderRadiusPreview/JS/index.js
@@ -0,0 +1,16 @@
+let valores = document.querySelectorAll('input')
+let divCss = document.querySelector('#css')
+let checkCss = document.querySelector('#css1')
+let checkMoz = document.querySelector('#moz')
+let checkWeb = document.querySelector('#web')
+valores[0] = document.querySelector('#v').onkeyup = () => {
+	// valores[0].style.border-radius = '40px'
+
+	divCss.style.borderTopLeftRadius = `${ valores[0].value }px`
+	divCss.style.borderTopRightRadius = `${ valores[1].value }px`
+	divCss.style.borderBottomLeftRadius = `${ valores[2].value }px`
+	divCss.style.borderBottomRightRadius = `${ valores[3].value }px`
+	if (checkCss.checked){
+		divCss.innerHTML += `${valores[0].value} &space`
+	}
+}
